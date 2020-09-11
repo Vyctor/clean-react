269373626bf8bf0920cf22a82d29629a8c12b12a
Vyctor <dev.vyctor@gmail.com>
2020-09-11 09:53:24 -0300

    refactor: organize imports

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import faker from 'faker';
+import { HttpPostParams } from '@/data/protocols/http/http-post-client';
 import { AxiosHttpClient } from './axios-http-client';
-import { HttpPostParams } from '../../../data/protocols/http/http-post-client';
 
 jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
